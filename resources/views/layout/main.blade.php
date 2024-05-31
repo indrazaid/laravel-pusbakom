d0fdb1d59907b10e3c428d4b12eaeab9c029e26b
indraZaid <irawanindra405@gmail.com>
2024-05-31 09:54:22 +0700

    atur url css js

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -14,17 +14,17 @@
    <link rel="shortcut icon" type="image/x-icon" href="assets/img/logo/favicon.png">
 
    <!-- CSS here -->
-   <link rel="stylesheet" href="assets/css/bootstrap.css">
-   <link rel="stylesheet" href="assets/css/meanmenu.css">
-   <link rel="stylesheet" href="assets/css/animate.css">
-   <link rel="stylesheet" href="assets/css/swiper-bundle.css">
-   <link rel="stylesheet" href="assets/css/slick.css">
-   <link rel="stylesheet" href="assets/css/nouislider.css">
-   <link rel="stylesheet" href="assets/css/magnific-popup.css">
-   <link rel="stylesheet" href="assets/css/font-awesome-pro.css">
-   <link rel="stylesheet" href="assets/css/flaticon.css">
-   <link rel="stylesheet" href="assets/css/spacing.css">
-   <link rel="stylesheet" href="assets/css/main.css">
+   <link rel="stylesheet" href="{{asset('assets/css/bootstrap.css')}}">
+   <link rel="stylesheet" href="{{asset('assets/css/meanmenu.css')}}">
+   <link rel="stylesheet" href="{{asset('assets/css/animate.css')}}">
+   <link rel="stylesheet" href="{{asset('assets/css/swiper-bundle.css')}}">
+   <link rel="stylesheet" href="{{asset('assets/css/slick.css')}}">
+   <link rel="stylesheet" href="{{asset('assets/css/nouislider.css')}}">
+   <link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}">
+   <link rel="stylesheet" href="{{asset('assets/css/font-awesome-pro.css')}}">
+   <link rel="stylesheet" href="{{asset('assets/css/flaticon.css')}}">
+   <link rel="stylesheet" href="{{asset('assets/css/spacing.css')}}">
+   <link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
 </head>
 
 <body>
@@ -392,19 +392,19 @@
    </div>
    <!-- footer  area end -->
    <!-- JS here -->
-   <script data-cfasync="false" src="../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="assets/js/vendor/jquery.js"></script>
-   <script src="assets/js/vendor/waypoints.js"></script>
-   <script src="assets/js/bootstrap-bundle.js"></script>
-   <script src="assets/js/meanmenu.js"></script>
-   <script src="assets/js/swiper-bundle.js"></script>
-   <script src="assets/js/magnific-popup.js"></script>
-   <script src="assets/js/nice-select.js"></script>
-   <script src="assets/js/counterup.js"></script>
-   <script src="assets/js/wow.js"></script>
-   <script src="assets/js/isotope-pkgd.js"></script>
-   <script src="assets/js/imagesloaded-pkgd.js"></script>
-   <script src="assets/js/ajax-form.js"></script>
-   <script src="assets/js/main.js"></script>
+   <script data-cfasync="false" src="{{asset('../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js')}}"></script><script src="{{asset('assets/js/vendor/jquery.js')}}"></script>
+   <script src="{{asset('assets/js/vendor/waypoints.js')}}"></script>
+   <script src="{{asset('assets/js/bootstrap-bundle.js')}}"></script>
+   <script src="{{asset('assets/js/meanmenu.js')}}"></script>
+   <script src="{{asset('assets/js/swiper-bundle.js')}}"></script>
+   <script src="{{asset('assets/js/magnific-popup.js')}}"></script>
+   <script src="{{asset('assets/js/nice-select.js')}}"></script>
+   <script src="{{asset('assets/js/counterup.js')}}"></script>
+   <script src="{{asset('assets/js/wow.js')}}"></script>
+   <script src="{{asset('assets/js/isotope-pkgd.js')}}"></script>
+   <script src="{{asset('assets/js/imagesloaded-pkgd.js')}}"></script>
+   <script src="{{asset('assets/js/ajax-form.js')}}"></script>
+   <script src="{{asset('assets/js/main.js')}}"></script>
 </body>
 
 
