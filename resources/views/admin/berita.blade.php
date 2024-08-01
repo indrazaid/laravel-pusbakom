27b6d0893fc86850103ca289d6ef398c190fbb02
indraZaid <irawanindra405@gmail.com>
2024-08-01 07:44:42 +0700

    replace

---
+++ resources/views/admin/berita.blade.php
@@ -0,0 +1,18 @@
+@extends('admin.main-admin')
+ @section('konten')
+
+
+
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+      <h1 class="h2">Berita</h1>
+      
+    </div>
+
+   
+    <h2>Selamat Datang di Laman Berita</h2>
+
+   
+    
+ 
+
+  @endsection
